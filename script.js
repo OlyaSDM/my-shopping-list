0beed265842533f98288cc5fd8ce7bfe941a2a34
Olya2125 <139589012+Olya2125@users.noreply.github.com>
2024-10-30 15:42:13 +0300

    added

---
+++ script.js
@@ -0,0 +1,49 @@
+const synth = window.speechSynthesis;
+
+const inputBox = document.querySelector('#input-box');
+const listContainer = document.querySelector('#list-container');
+
+inputBox.addEventListener('keypress', function (e){
+  if(e.key === "Enter"){
+    addTask()
+  }
+})
+
+function addTask(){
+  if(inputBox.value === ''){
+    alert('You must write something.');
+  }
+  else{
+    let li = document.createElement('li');
+    li.innerHTML = inputBox.value;
+    listContainer.append(li);
+    let span = document.createElement('span');
+    span.innerHTML = '🛒';
+    li.appendChild(span)
+  }
+  inputBox.value = '';
+  saveData()
+}
+listContainer.addEventListener('click', function(e){
+  if(e.target.tagName === 'LI'){
+    e.target.classList.toggle('checked');
+    saveData();
+  }
+  else if(e.target.tagName === 'SPAN'){
+    e.target.parentElement.remove();
+    let yellThis = new SpeechSynthesisUtterance('Good job!');
+    synth.speak(yellThis);
+    saveData();
+  }
+}, false)
+
+function saveData(){
+  localStorage.setItem('data', listContainer.innerHTML)
+}
+function showTask(){
+  listContainer.innerHTML = localStorage.getItem('data');
+}
+showTask();
+
+gsap.from("h1", {y: -80, delay: 0.6, opacity: 0.5, duration: 1, ease: "bounce.out"})
+
